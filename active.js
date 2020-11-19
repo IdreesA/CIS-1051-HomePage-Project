e7198cbdb267a23db424e831bb09ffd40f9ab77f
IdreesA <idrees.philly@gmail.com>
2020-11-19 16:29:52 -0500

    working on calculator

---
+++ active.js
@@ -0,0 +1,7 @@
+let calcbutton = document.querySelector('#CalcSubmit');
+let body = document.querySelector('body');
+let num1 = document.querySelector('#num1');
+let num2 = document.querySelector('#num2');
+
+
+
