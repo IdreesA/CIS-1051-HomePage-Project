f94c90ef983a42d559d118396afe2e401c964744
IdreesA <idrees.philly@gmail.com>
2020-11-20 02:36:10 -0500

    covered most of calculator functionality

--- active.js
+++ active.js
@@ -1,7 +1,10 @@
+
+/*
 let calcbutton = document.querySelector('#CalcSubmit');
 let body = document.querySelector('body');
 let num1 = document.querySelector('#num1');
 let num2 = document.querySelector('#num2');
 
 
+*/
 
